ee19f4d2e1031c4ec4c23ac902d4d5ea3cf159ec
zoisper <zoisper@gmail.com>
2022-05-12 15:05:55 +0100

    wip

--- 4/Barreira.java
+++ 4/Barreira.java
@@ -1,35 +1,23 @@
 import java.util.concurrent.Semaphore;
 
 class Barreira {
-    private final int N;
-    private int c = 0;
-    private Semaphore mut = new Semaphore(1);
-    private Semaphore sem = new Semaphore(1);
+    int n;
+    int c = 0;
+    Semaphore mut = new Semaphore(1);
+    Semaphore sem = new Semaphore(0);
 
-    public Barreira(int N){
-        this.N = N;
+
+    Barreira (int N) { 
+        this.n = N;
     }
-
-    public void await() throws InterruptedException{
+    void await() throws InterruptedException { 
         mut.acquire();
         c+=1;
         int v = c;
         mut.release();
-        
-        /*if(v<N){
-            sem.acquire();
-
-        }
-        
-        else{
-            for(int i=0; i<N-1; i++)
-                sem.release();
-        }*/
-
-        if(v==N)
-            for(int i=0;i<N;i++)
+        if(v==n)
+            for(int i=0; i<n ; i++)
                 sem.release();
         sem.acquire();
-    }
-    
+     }
 }
--- 4/BoundedBuffer.java
+++ 4/BoundedBuffer.java
@@ -1,56 +1,47 @@
 import java.util.concurrent.Semaphore;
 import java.util.Random;
 
-class BoundedBuffer{
-    private int[] buf;
-    private int iget = 0;
-    private int iput = 0;
-    
+class BoundedBuffer<T> {
+    T buf[];
+    int iget = 0;
+    int iput = 0;
     Semaphore items;
     Semaphore slots;
     Semaphore mutget;
-    Semaphore mutput;  
+    Semaphore mutput;
 
     BoundedBuffer(int N) { 
-
-        this.buf = new int[N];
+        this.buf =  (T[]) new Object[N];
         this.items = new Semaphore(0);
         this.slots = new Semaphore(N);
         this.mutget = new Semaphore(1);
-        this.mutput = new Semaphore(1);
-        
+        this.mutput = new Semaphore(1); 
      }
     
-     public int get() throws InterruptedException { 
-        int res;
+     T get() throws InterruptedException { 
+        T res;
         items.acquire();
         mutget.acquire();
-        res = buf[iget];
+        res = (T) buf[iget];
         iget = (iget+1) % buf.length;
         mutget.release();
         slots.release();
         return res;
      }
-
-
-    public void put(int v) throws InterruptedException {
+    void put(T x) throws InterruptedException { 
         slots.acquire();
         mutput.acquire();
-        buf[iput] =  v;
-        iput = (iput+1) % buf.length;
+        buf[iput] = x;
+        iput = (iput +1) % buf.length;
         mutput.release();
         items.release();
-
      }
-
 }
 
-
-
-class Teste{
+class Main{
     public static void main(String[] args){
         
-       BoundedBuffer b = new BoundedBuffer(20);
+       BoundedBuffer<Integer> b = new BoundedBuffer<>(20);
        
        new Thread(() -> {
            try{   
