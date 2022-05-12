0922c5e074af42a1e50657b5451f8de3bec265b7
zoisper <zoisper@gmail.com>
2022-05-12 11:51:46 +0100

    wip

--- 8/login_manager.erl
+++ 8/login_manager.erl
@@ -1,4 +1,4 @@
--module(login_manager).
+-module(my_login_manager).
 -export([start/0,
         cretate_account/2,
         close_account/2,
@@ -8,43 +8,69 @@
 
 start() -> register(?MODULE, spawn(fun() -> loop(#{}) end)).
 
-invoke(Request) ->
-    ?MODULE ! {Request, self()},
-    receive {Res, ?MODULE} -> Res end.
+cretate_account(User, Pass) -> invoke({create_account, User, Pass}).
 
-cretate_account (User, Pass) -> invoke({create_accoubt, User, Pass}).
-close_account(User, Pass) -> invoke({close_account, User, Pass}).
-login(User,Pass) -> invoke({login, User,Pass}).
-logout(User) -> invoke({logout, User}).
+close_account(User, Pass) -> invoke({close_account,User, Pass}).
+
+login(User, Pass) -> invoke({login,User, Pass}).
+
+logout(User) -> invoke({logout,User}).
+
 online() -> invoke(online).
 
+invoke(Request) -> ?MODULE ! {Request,self()},
+receive {Response, ?MODULE} -> Response end .
+
 loop(Map) ->
-    receive
-        {{cretate_account, User, Pass},Form} -> 
-            case maps:is_key(User, Map) of 
+    receive 
+        {{create_account, User, Pass},From} -> 
+            case maps:is_key(User,Map) of
                 true -> 
                     From ! {user_exists, ?MODULE},
                     loop(Map);
-                false -> 
+                false ->
                     From ! {ok, ?MODULE},
-                    loop(maps:put(User, {Pass, false}, Map))
-            end;
-        {{close_account, User, Pass}, From} ->
-            case maps:find(User, Map) of
+                    loop(maps:put(User,{Pass, false},Map)) 
+                end;
+        {{close_account,User, Pass},From} ->
+            case maps:find(User,Map) of
                 {ok,{Pass,_}} ->
                     From ! {ok, ?MODULE},
-                    loop(maps:remove(User, Map));
+                    loop(maps:remove(User,Map));
+                _-> 
+                    From ! {invalid, ?MODULE},
+                    loop(Map)
+                end;
+        {{login,User, Pass},From} ->
+            case maps:find(User, Map) of
+                {ok,{Passw,_}} ->
+                    if Pass == Passw ->
+                        From ! {ok, ?MODULE},
+                        loop(maps:update(User,{Pass,true},Map));
+                    true -> From ! {invalid, ?MODULE},
+                            loop(Map)
+                            end;
                 _->
                     From ! {invalid, ?MODULE},
                     loop(Map)
                 end;
+        {logout,User,From} ->
+            case maps:find(User,Map) of
+                {ok,{Pass,_}} ->
+                    From ! {ok,?MODULE},
+                    loop(maps:update(User,{Pass,false},Map));
+                _->
+                    {invalid, ?MODULE},
+                    loop(Map)
+            end;
         {online, From} ->
-            F = fun(User, {_Pass, true}, Acc) -> [User | Acc];
-                   (_User, _, Acc) -> Acc;
-                end,
-            Res = maps:fold(F,[], Map),
-            Res = [U || {U, {_P, true}} <- maps:to_list(Map)],
+            Response = [User ||{User,{_,State}} <- maps:to_list(Map), State == true],
+            From ! Response,
+            loop(Map)
+        end.
 
-            From ! {Res, ?MODULE},
-            loop(Map);
-    end.
+        
+
+
+
+
